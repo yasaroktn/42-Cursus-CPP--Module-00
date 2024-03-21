d93a9b747a09fd6f5a7677bb30437d10ef1fd6f7
Yaşar Ökten <yasaroktnn@gmail.com>
2024-03-21 07:16:34 +0300

    finish

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,32 +1,16 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yokten <yokten@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/16 15:52:08 by yokten            #+#    #+#             */
-/*   Updated: 2024/01/16 17:26:14 by yokten           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <iostream>
 
-int	main(int	ac,	char	**av)
-{
-	int i = 0;
-	int j = 0;
-
+int	main(int ac, char **av) {
 	if (ac == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else if (ac > 1)
-	{
-		while (++i < ac)
-		{
-			while (av[i][j])
-				std::cout << (char)toupper(av[i][j++]);
-			j = 0;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else {
+		for (int i = 1; av[i]; i++) {
+			for (int j = 0; av[i][j]; j++)
+				std::cout << (char) toupper(av[i][j]);
+			if (i < ac - 1)
+				std::cout << ' ';
 		}
-		std::cout << std::endl;
 	}
+	std::cout << std::endl;
+	return (EXIT_SUCCESS);
 }
---
+++ ex01/PhoneBook.hpp
@@ -0,0 +1,20 @@
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+
+#include "Contact.hpp"
+
+class PhoneBook
+{
+private:
+    Contact     _contacts[8];
+    int         _readInput(void) const;
+public:
+    PhoneBook();
+    ~PhoneBook();
+    void    addContact(void);
+    void    printContacts(void) const;
+    void    search(void) const;
+    void    welcome(void) const;
+};
+
+#endif // PHONEBOOK_HPP
